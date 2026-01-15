9374984784a22b6099b293707879696eb9da48d9
adriann <a267336@dac.unicamp.br>
2026-01-14 21:48:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+let carrinho = [];
+
+document.querySelectorAll('.btn-comprar').forEach(btn => {
+    btn.addEventListener('click', function() {
+        const produto = this.closest('.produto');
+        const nome = produto.querySelector('h3').textContent;
+        const preco = produto.querySelector('.preco').textContent;
+        
+        carrinho.push({ nome, preco });
+        alert(`${nome} adicionado ao carrinho!`);
+        console.log('Carrinho:', carrinho);
+    });
+});
